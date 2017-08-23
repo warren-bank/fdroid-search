7dea9537e136cd270832b059f8820b27bde47994
Warren R. Bank <warren.r.bank@gmail.com>
2017-08-23 07:44:29 -0700

    v2.3.1: pre-populate search input field from URL hash

--- js/fdroid-search-autocomplete.js
+++ js/fdroid-search-autocomplete.js
@@ -82,6 +82,7 @@
         // pre-populate the search input field from the URL #hash:
         if (window.location.hash && (window.location.hash.length > (1 + config.minChars))) {
           autocomplete.input.value = decodeURIComponent(window.location.hash).substr(1);
+          history.replaceState(undefined, undefined, window.location.href.replace(/#.*$/, ''));
           display_search_results(1);
         }
 
